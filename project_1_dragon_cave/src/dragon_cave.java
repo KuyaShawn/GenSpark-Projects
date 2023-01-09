5096e458d887a175212f10ce9ceab0c0aa1173e1
KuyaShawn <EashuneAbenojar@hotmail.com>
2023-01-09 13:55:31 -0800

    Removed the extra println and made it look cleaner

--- project_1_dragon_cave/src/dragon_cave.java
+++ project_1_dragon_cave/src/dragon_cave.java
@@ -6,10 +6,10 @@ public class dragon_cave {
 
         // Prints dialog into console
         System.out.println("You are in a land full of dragons. In front of you," + '\n' +
-        "you see two caves. In one cave, the dragon is friendly" + '\n' +
-        "and will share his treasure with you. The other dragon" + '\n' +
-        "is greedy and hungry and will eat you on sight." + '\n' +
-        "which cave will you go into? (1 or 2)");
+                            "you see two caves. In one cave, the dragon is friendly" + '\n' +
+                            "and will share his treasure with you. The other dragon" + '\n' +
+                            "is greedy and hungry and will eat you on sight." + '\n' +
+                            "which cave will you go into? (1 or 2)");
 
         // reads user's input
         int caveChoice = userInput.nextInt();
@@ -18,14 +18,14 @@ public class dragon_cave {
         // user input's choice code.
         if(caveChoice == 1){
             System.out.println("You approach the cave..." + '\n' +
-            "It is dark and spooky..." + '\n' +
-            "A large dragon jumps out in front of you! He opens his jaws and..." + '\n' +
-            "Gobbles you down in one bite!");
+                                "It is dark and spooky..." + '\n' +
+                                "A large dragon jumps out in front of you! He opens his jaws and..." + '\n' +
+                                "Gobbles you down in one bite!");
         }else if(caveChoice == 2) {
             System.out.println("You approach the cave..." + '\n' +
-            "It is dark and spooky..." + '\n' +
-            "A large dragon jumps out in front of you! He opens his jaws and..." + '\n' +
-            "Asks you to take his treasure!");
+                                "It is dark and spooky..." + '\n' +
+                                "A large dragon jumps out in front of you! He opens his jaws and..." + '\n' +
+                                "Asks you to take his treasure!");
         }else {
             System.out.println("Invalid cave choice");
         }
