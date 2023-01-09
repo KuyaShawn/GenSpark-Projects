805effa3e5ccbabf48a8515c3b95a4cae0cca893
KuyaShawn <EashuneAbenojar@hotmail.com>
2023-01-09 13:45:13 -0800

    Added User input (Scanner class) with output dialog to whichever the user chooses as a cave.

--- project_1_dragon_cave/src/Main.java
+++ project_1_dragon_cave/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner userInput = new Scanner(System.in);
+
+        // Prints dialog into console
+        System.out.println("You are in a land full of dragons. In front of you,");
+        System.out.println("you see two caves. In one cave, the dragon is friendly");
+        System.out.println("and will share his treasure with you. The other dragon");
+        System.out.println("is greedy and hungry and will eat you on sight.");
+        System.out.println("which cave will you go into? (1 or 2)");
+
+        // reads user's input
+        int caveChoice = userInput.nextInt();
+        System.out.println();
+
+        // user input's choice code.
+        if(caveChoice == 1){
+            System.out.println("You approach the cave...");
+            System.out.println("It is dark and spooky...");
+            System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+            System.out.println("Gobbles you down in one bite!");
+        }else if(caveChoice == 2) {
+            System.out.println("You approach the cave...");
+            System.out.println("It is dark and spooky...");
+            System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+            System.out.println("Asks you to take his treasure!");
+        }else {
+            System.out.println("Invalid cave choice");
+        }
     }
 }
