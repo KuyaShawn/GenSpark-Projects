1b57a38cc59ba0d333bb76ec9c37a4651d9c272b
KuyaShawn <EashuneAbenojar@hotmail.com>
2023-01-11 11:47:01 -0800

    Added Try Catch
    Implemented a try catch block for each input the user uses.

--- project_2_guess_the_number/src/guess_the_number.java
+++ project_2_guess_the_number/src/guess_the_number.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
 import java.util.Random;
+
 public class guess_the_number {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         Random rand = new Random();
+        String usersName = "";
         int guessCount = 0;
         String replayChoice;
 
-        //Prints a dialogue into console
-        System.out.println("Hello! What is your name?");
+        try {
+            //Prints a dialogue into console
+            System.out.println("Hello! What is your name?");
 
-        // reads the user's name as the input
-        String usersName = userInput.nextLine();
+            // reads the user's name as the input
+            usersName = userInput.nextLine();
+            //Checks if user's input is empty or is invalid.
+
+            if (usersName.isEmpty()) {
+                throw new IllegalArgumentException("Caught Exception: Invalid Name.");
+            } else if (!usersName.matches("[a-zA-Z]+")) {
+                throw new IllegalArgumentException("Caught Exception: Invalid Name.");
+            }
+
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+
 
         /* loops if user's number doesn't match the computer's number
            otherwise it breaks off and continue.
@@ -24,31 +39,39 @@ public class guess_the_number {
                     "take a guess.");
 
             while (true) {
-                // reads the user's inputted number
-                int usersNumGuess = userInput.nextInt();
-                //Checks if user's number is too high, low, or just right to continue the game
-                if(usersNumGuess > randNum){
-                    System.out.println("Your Guess is too high." + '\n'
-                    + "Take a guess.");
-                    guessCount++;
-                } else if (usersNumGuess < randNum) {
-                    System.out.println("Your Guess is too low." + '\n'
-                    + "Take a guess.");
-                    guessCount++;
-                }else {
-                    guessCount++;
-                    System.out.println("Good job, " + usersName + "! You guessed my number in " + guessCount + " guesses!");
-                    break;
+                try {
+                    // reads the user's inputted number
+                    int usersNumGuess = userInput.nextInt();
+                    //Checks if user's number is too high, low, or just right to continue the game
+                    if (usersNumGuess > randNum) {
+                        System.out.println("Your Guess is too high." + '\n'
+                                + "Take a guess.");
+                        guessCount++;
+                    } else if (usersNumGuess < randNum) {
+                        System.out.println("Your Guess is too low." + '\n'
+                                + "Take a guess.");
+                        guessCount++;
+                    } else {
+                        guessCount++;
+                        System.out.println("Good job, " + usersName + "! You guessed my number in " + guessCount + " guesses!");
+                        break;
+                    }
+                } catch (NumberFormatException e) {
+                    throw new NumberFormatException("Caught Exception: Invalid input.");
                 }
             }
-            // Asks the user for their input if they want to continue playing or not
-            System.out.println("Would you like to play again? (Y or N)");
-            replayChoice = userInput.next();
+            try {
+                // Asks the user for their input if they want to continue playing or not
+                System.out.println("Would you like to play again? (Y or N)");
+                replayChoice = userInput.next();
+            }catch (Exception e){
+                throw new IllegalArgumentException("Caught Exception: Invalid Choice.");
+            }
 
             /* if they wish to continue, then inputting "y" will instantiate the game again.
              If "n" then the game will close
              */
-        }while (replayChoice.equalsIgnoreCase("y"));
+        } while (replayChoice.equalsIgnoreCase("y"));
 
     }
 }
