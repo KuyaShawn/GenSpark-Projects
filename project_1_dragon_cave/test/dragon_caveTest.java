a406a1a7bf8c110af4836783b58145ce755ef510
KuyaShawn <EashuneAbenojar@hotmail.com>
2023-01-12 22:01:07 -0800

    refined the unit testing
    Added assert equals for each test and assert throws for exception testing. while also adding clear msg's for the tests, and having each test pass

--- project_1_dragon_cave/test/dragon_caveTest.java
+++ project_1_dragon_cave/test/dragon_caveTest.java
@@ -13,11 +13,12 @@ class dragon_caveTest {
 
     @Test
     void introDialog() {
-        String expectedDialog = "You are in a land full of dragons. In front of you," + '\n' +
+        String expectedIntroDialog = "You are in a land full of dragons. In front of you," + '\n' +
                 "you see two caves. In one cave, the dragon is friendly" + '\n' +
                 "and will share his treasure with you. The other dragon" + '\n' +
                 "is greedy and hungry and will eat you on sight." + '\n' +
                 "which cave will you go into? (1 or 2)";
+        assertEquals(expectedIntroDialog, dragonCave.introDialog(),"Error on introDialog method in the return msg");
     }
 
     @Test
@@ -26,6 +27,7 @@ class dragon_caveTest {
                 "It is dark and spooky..." + '\n' +
                 "A large dragon jumps out in front of you! He opens his jaws and..." + '\n' +
                 "Gobbles you down in one bite!";
+        assertEquals(expectedChoice1, dragonCave.getCaveChoice(1), "Error on getCaveChoice for cave choice 1");
     }
 
     @Test
@@ -34,15 +36,13 @@ class dragon_caveTest {
                 "It is dark and spooky..." + '\n' +
                 "A large dragon jumps out in front of you! He opens his jaws and..." + '\n' +
                 "Asks you to take his treasure!";
+        assertEquals(expectedChoice2, dragonCave.getCaveChoice(2), "Error on getCaveChoice for cave choice 2");
     }
-/*
+
     @Test
     void getCaveChoice3() throws NumberFormatException {
-        assertThrows(NumberFormatException.class, dragonCave.getCaveChoice(10), )
+        assertThrows(NumberFormatException.class, () -> dragonCave.getCaveChoice(10), "Check getCaveChoice method or Try catch for error" );
     }
-
- */
-
 
     @AfterEach
     void tearDown() {
