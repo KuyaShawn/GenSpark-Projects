110666c7ef0f557e0fa9cdf529e1f131d6cc3253
KuyaShawn <EashuneAbenojar@hotmail.com>
2023-01-13 08:48:26 -0800

    Created JUnit test  and added methods
    Made a Unit test for the guessing game & created/refined methods for the Unit Testing

---
+++ project_2_guess_the_number/test/guess_the_numberTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class guess_the_numberTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+
+    @Test
+    void introDialogue() {
+    }
+
+    @Test
+    void isNameValid() {
+    }
+
+    @Test
+    void replayGame() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
