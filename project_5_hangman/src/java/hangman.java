675cddda0c0ee11da25337d9374345b0860830a0
KuyaShawn <EashuneAbenojar@hotmail.com>
2023-01-23 13:32:18 -0800

    Hangman improvments
    Added a text file filled with words, added code for the starting dialog, Added code for getting player two's name, and added code for replaying the game

--- project_5_hangman/src/java/hangman.java
+++ project_5_hangman/src/java/hangman.java
@@ -1,35 +1,47 @@
 import java.util.*;
 
 public class hangman {
-    public static void main(String[] args) {
 
+    // Starting Dialog for the Hangman Game
+    public static String startingDialog() {
+        String startingDialog = "Welcome to the Hangman game, you will be given a number by a computer and all you " +
+                "have to do is guess the word, good luck!";
+
+        // returns the dialog for starting the game
+        return startingDialog;
     }
 
     // Player one is the computer who guesses the word for player two
-    public static String getPlayerOne(){
-
+    public static String getPlayerOne() {
 
         return null;
     }
 
     // creates a player for guessing the word given by the computer
-    public static String getPlayerTwo(){
+    public static String getPlayerTwo() {
         // code for getting player name
+        Scanner userInput = new Scanner(System.in);
 
+        System.out.println("Hello please Input your name!");
+        String playerTwoName = userInput.nextLine();
+
+        if (playerTwoName.isEmpty()) {
+            throw new IllegalArgumentException("Caught Exception: Invalid Name.");
+        }
+        if (!playerTwoName.matches("[a-zA-Z]+")) {
+            throw new IllegalArgumentException("Caught Exception: Invalid Name.");
+        }
 
         // return Name
-        return null;
+        return playerTwoName;
     }
 
+    public void printHangingMan() {
+        // code for printing off the hangman
 
-    public static String startingDialog(){
-        // code for starting the game
-
-        // returns the dialog for starting the game
-        return null;
     }
 
-    public static ArrayList<String> missingLetters(){
+    public static ArrayList<String> missingLetters() {
         // code for missing letters
 
         // returns a string of arrays that the player is missing
@@ -37,19 +49,47 @@ public class hangman {
     }
 
 
-    public static String playerGuesses(){
+    public static String inputGuess(char guessLetter, ArrayList<String> word) {
 
+        if (word.contains(guessLetter)) {
+            // Add stuff here
+        }
         // returns a string if player guesses right or not
         return null;
     }
 
-    public static boolean replayGame(){
+    public static boolean replayGame() {
         // code for replaying the game
+        boolean playAgain = false;
+        do {
+            try {
+                String replayChoice;
+                Scanner userInput = new Scanner(System.in);
+                // Asks the user for their input if they want to continue playing or not
+                System.out.println("Do you want to play again? (yes or no)");
+                replayChoice = userInput.nextLine();
 
+                if (replayChoice.equalsIgnoreCase("yes")) {
+                    playAgain = true;
+                } else if (replayChoice.equalsIgnoreCase("no")) {
+                    return false;
+                } else {
+                    throw new IllegalArgumentException();
+                }
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("Caught Exception: Invalid Choice.");
+            }
+            // stop when playAgain is true or returned out of the loop
+        } while (!playAgain);
 
         // return T or F if player wants to replay the game
-        return false;
+        return playAgain;
     }
 
+    public static void main(String[] args) {
+        System.out.println(startingDialog());
+        System.out.println(getPlayerTwo());
 
+    }
 }
