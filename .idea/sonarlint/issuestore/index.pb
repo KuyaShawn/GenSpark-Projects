17f87cca887e76fdfba4427985b16768a1ad7d14
KuyaShawn <EashuneAbenojar@hotmail.com>
2023-01-23 11:27:27 -0800

    Create Project 5
    Project 5 is a hangman game. I've created the JAVA file & Test file for the hangman game. And added some skeleton code to brainstorm ideas

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,7 +5,3 @@ _
 /project_1_dragon_cave/src/java/dragon_cave.java,b\9\b92de245abb389ec6f553fbf4c0ecb66a62edf93
 _
 /project_1_dragon_cave/test/dragon_caveTest.java,2\a\2a4bf9282e5e7cf18d856bfc3558454d89662020
-i
-9project_2_guess_the_number/test/guess_the_numberTest.java,9\c\9c4bec7eb9edb7f94ba3cdb443d62717b599e48c
-i
-9project_2_guess_the_number/src/java/guess_the_number.java,8\c\8c22fcadab2ae4c67bbdcbbe582984293c18add3
---
+++ project_5_hangman/src/java/hangman.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class hangman {
+    public static void main(String[] args) {
+
+    }
+
+    // Player one is the computer who guesses the word for player two
+    public static String getPlayerOne(){
+
+
+        return null;
+    }
+
+    // creates a player for guessing the word given by the computer
+    public static String getPlayerTwo(){
+        // code for getting player name
+
+
+        // return Name
+        return null;
+    }
+
+
+    public static String startingDialog(){
+        // code for starting the game
+
+        // returns the dialog for starting the game
+        return null;
+    }
+
+    public static ArrayList<String> missingLetters(){
+        // code for missing letters
+
+        // returns a string of arrays that the player is missing
+        return null;
+    }
+
+
+    public static String playerGuesses(){
+
+        // returns a string if player guesses right or not
+        return null;
+    }
+
+    public static boolean replayGame(){
+        // code for replaying the game
+
+
+        // return T or F if player wants to replay the game
+        return false;
+    }
+
+
+}
