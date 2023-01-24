eb7113d1cfbe70125f0b116364cb5835f7d71c4a
KuyaShawn <EashuneAbenojar@hotmail.com>
2023-01-24 13:48:53 -0800

    Major improvement
    Added a word list, created a random word generator for the computer to choose from, updated the printingHangMan method, & added some setter and getters

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,6 +2,4 @@
 _
 /project_1_dragon_cave/project_1_dragon_cave.iml,1\8\1823020e32db32b1d15bfd7db572608dffeb6fcf
 _
-/project_1_dragon_cave/src/java/dragon_cave.java,b\9\b92de245abb389ec6f553fbf4c0ecb66a62edf93
-_
 /project_1_dragon_cave/test/dragon_caveTest.java,2\a\2a4bf9282e5e7cf18d856bfc3558454d89662020
--- project_5_hangman/src/java/hangman.java
+++ project_5_hangman/src/java/hangman.java
@@ -1,11 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class hangman {
+    private static String randomWord;
 
     // Starting Dialog for the Hangman Game
     public static String startingDialog() {
-        String startingDialog = "Welcome to the Hangman game, you will be given a number by a computer and all you " +
-                "have to do is guess the word, good luck!";
+        String startingDialog = "Welcome to the Hangman game, you will be given a word by a computer and all you " +
+                "have to do is guess the word with, good luck! \n" +
+                "\nH A N G M A N\n" +
+                "   +---+\n" +
+                "       |\n" +
+                "       |\n" +
+                "       |\n" +
+                " ========\n";
 
         // returns the dialog for starting the game
         return startingDialog;
@@ -13,8 +22,35 @@ public class hangman {
 
     // Player one is the computer who guesses the word for player two
     public static String getPlayerOne() {
+        // Create a Random object to generate a random index
+        Random random = new Random();
+        String fileName = "C:/Users/Big Daddy/Documents/GitHub/GenSpark-Projects/project_5_hangman/src/resources/wordlist.txt";
+        // Declares an empty List of strings called "words"
+        List<String> words = new ArrayList<>();
 
-        return null;
+        // Try to read from the file
+        try {
+            // Create a File object from the fileName
+            File file = new File(fileName);
+            // Create a Scanner object to read the file
+            Scanner readFile = new Scanner(file);
+
+            // While there is another line in the file, add it to the list of words
+            while (readFile.hasNextLine()) {
+                words.add(readFile.nextLine());
+            }
+            // close the Scanner for reading the file
+            readFile.close();
+        } catch (FileNotFoundException e) {
+            // if the file is not found, print the stack traces of the exception
+            System.out.println("Caught Exception: Invalid Choice.");
+        }
+
+        // Generate a random index between 0 and the number of words in the word list
+        int randomIndex = random.nextInt(words.size());
+
+        // returns the random word given
+        return words.get(randomIndex);
     }
 
     // creates a player for guessing the word given by the computer
@@ -36,13 +72,72 @@ public class hangman {
         return playerTwoName;
     }
 
-    public void printHangingMan() {
+    public void printHangingMan(int wrongGuesses) {
+        // TODO: 1/24/2023 construct something that doesn't use if statement for printing the hangman
+        String givenWord = getRandomWord();
+        
+        
         // code for printing off the hangman
+        if (wrongGuesses > 0) {
+            System.out.println(
+
+                    "   +---+\n" +
+                            "   O  |\n" +
+                            "       |\n" +
+                            "       |\n" +
+                            " ========\n");
+        }
+        if (wrongGuesses > 1) {
+            System.out.println(
+
+                    "   +---+\n" +
+                            "   O    |\n" +
+                            "  /|\\  |\n" +
+                            "      |\n" +
+                            " ========\n");
+
+        }
+        if (wrongGuesses > 2) {
+            System.out.println(
+                    "   +---+\n" +
+                            "   O    |\n" +
+                            "  /|\\  |\n" +
+                            "   |    |\n" +
+                            " ========\n");
+        }
+        if (wrongGuesses > 3) {
+            System.out.println(
+                    "   +---+\n" +
+                            "   O    |\n" +
+                            "  /|\\  |\n" +
+                            "   |    |\n" +
+                            " ========\n");
+        }
+        if (wrongGuesses > 4) {
+            System.out.println(
+                    "   +---+\n" +
+                            "   O    |\n" +
+                            "  /|\\  |\n" +
+                            "   |    |\n" +
+                            "   |    |\n" +
+                            " ========\n");
+        }
+        if (wrongGuesses > 5) {
+            System.out.println(
+                    "   +---+\n" +
+                            "   O    |\n" +
+                            "  /|\\  |\n" +
+                            "   |    |\n" +
+                            "   |    |\n" +
+                            "  / \\    |\n" +
+                            " ========\n");
+        }
 
     }
 
-    public static ArrayList<String> missingLetters() {
-        // code for missing letters
+    public static ArrayList<String> missingLetters(String guess, int wrongGuesses) {
+        // Declare a list to store the missing letters
+        ArrayList<String> missingLetter = new
 
         // returns a string of arrays that the player is missing
         return null;
@@ -87,9 +182,18 @@ public class hangman {
         return playAgain;
     }
 
+    public static String getRandomWord() {
+        return randomWord;
+    }
+
+    public void setRandomWord(String randomWord) {
+        this.randomWord = randomWord;
+    }
+
     public static void main(String[] args) {
         System.out.println(startingDialog());
-        System.out.println(getPlayerTwo());
+        //System.out.println(getPlayerTwo());
+        System.out.println(getPlayerOne());
 
     }
 }
