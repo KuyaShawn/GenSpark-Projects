962ef03d3c9a78032f2628e483aa01ea08253c95
KuyaShawn <EashuneAbenojar@hotmail.com>
2023-01-26 21:38:02 -0800

    Added Testing
    Added Unit Testing

---
+++ project_5_hangman/test/hangmanTest.java
@@ -0,0 +1,53 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.Assert.*;
+
+public class hangmanTest {
+
+    hangman hangman;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        hangman = new hangman();
+    }
+
+    @Test
+    public void startingDialog() {
+    }
+
+    @Test
+    public void generateWord() {
+    }
+
+    @Test
+    public void getPlayerTwo() {
+    }
+
+    @Test
+    public void printHangingMan() {
+    }
+
+    @Test
+    public void guessedList() {
+    }
+
+    @Test
+    public void replayGame() {
+    }
+
+    @Test
+    public void startGame() {
+    }
+
+    @Test
+    public void isWordGuessed() {
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+    }
+}
