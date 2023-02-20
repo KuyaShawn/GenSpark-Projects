38aef7d8f20d62310c4ffc8cf6db0ec5ba15cfc4
KuyaShawn <EashuneAbenojar@hotmail.com>
2023-02-19 21:33:32 -0800

    Added files
    populated project 8 with files from project 6 to start the Human Vs goblin GUI project

---
+++ project_8_human_vs_goblins_GUI/src/java/Human.java
@@ -0,0 +1,58 @@
+public class Human extends Creature {
+    String creatureType;
+    int strength;
+    int defence;
+    int health;
+    int luck;
+
+    public Human(String creatureType, int strength, int defence, int health, int luck) {
+        super(creatureType, strength, defence, health, luck);
+    }
+
+    // Apply equipment in the future
+    public void equipment(String equipment) {
+        switch (equipment.toLowerCase()) {
+            case "sword":
+                strength += 5;
+                break;
+            case "shield":
+                defence += 3;
+                break;
+            case "amulet":
+                health += 4;
+                luck += 4;
+                break;
+            case "ring":
+                health += 2;
+                luck += 2;
+                break;
+            case "armored chest":
+                defence += 4;
+                break;
+            case "armored leg":
+                defence += 4;
+                break;
+            case "armored shoe":
+                defence += 1;
+                break;
+            case "armored helmet":
+                defence += 3;
+                break;
+            case "armored gauntlet":
+                defence += 1;
+                strength += 1;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + equipment.toLowerCase());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Creature: " + getCreatureType() +
+                "\nHealth: " + getHealth() +
+                "\nStrength: " + getStrength() +
+                "\nDefence: " + getDefence() +
+                "\nLuck: " + getLuck() + "\n";
+    }
+}
---
+++ project_8_human_vs_goblins_GUI/src/java/Land.java
@@ -0,0 +1,78 @@
+public class Land {
+    int rows;
+    int columns;
+    int[][] gameBoard;
+
+    // UTF character for human & Goblin
+    private String humanUTF = "\uD83E\uDDCD";
+    private String goblinUTF = "\uD83D\uDC79";
+
+    public Land(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.gameBoard = new int[rows][columns];
+    }
+
+    // Generates a board for the game
+    public void createGameBoard() {
+        // try-catch block for handling any exceptions
+        try {
+            // iterate through a nested loop by going through each element of the gameBoard Array
+            // then fills each element with the appropriate value (UTF or symbol)
+            for (int y = 0; y < columns; y++) {
+                StringBuilder line = new StringBuilder();
+                line.append("|");
+                for (int x = 0; x < rows; x++) {
+                    if (gameBoard[x][y] == 0) {
+                        line.append("|   |");
+                    } else if (gameBoard[x][y] == 2) {
+                        line.append("| " + goblinUTF + " |");
+                    } else if (gameBoard[x][y] == 3) {
+                        line.append("| X |");
+                    } else {
+                        line.append("| " + humanUTF + " |");
+                    }
+                }
+                line.append("|");
+                System.out.println(line);
+            }
+            // creates a space between each of the game boards.
+            System.out.println("\n");
+        } catch (Exception e) {
+            System.out.println("Cannot create game Board.");
+        }
+    }
+
+    // set position for the player
+    public void setPostion(int x, int y) {
+        try {
+            gameBoard[x][y] = 1;
+        } catch (Exception e) {
+            System.out.println("Cannot set player position.");
+        }
+    }
+
+    // set death position for the creature
+    public void setDeath(int x, int y) {
+        try {
+            gameBoard[x][y] = 2;
+        } catch (Exception e) {
+            System.out.println("Cannot set death to creature.");
+        }
+    }
+
+    // set position for where the combat initiated
+    public void combatInitiated(int x, int y) {
+        try {
+            gameBoard[x][y] = 3;
+        } catch (Exception e) {
+            System.out.println("Cannot initiate combat.");
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "The Starting Board";
+    }
+}
